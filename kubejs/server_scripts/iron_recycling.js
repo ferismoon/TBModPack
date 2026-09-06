85ac05e155924c346f54230be6e19a1fd58a53e1
Mooncrow <4670963+ferismoon@users.noreply.github.com>
2026-09-06 12:57:23 +0100

    Removing Data Packs, KubeJS Updates, Mystical Elk has more spawn locations

---
+++ kubejs/server_scripts/iron_recycling.js
@@ -0,0 +1,21 @@
+// Recycle iron equipment into one ingot in either furnace type.
+ServerEvents.recipes(event => {
+  const recyclableItems = [
+    'iron_pickaxe', 'iron_shovel', 'iron_axe', 'iron_hoe', 'iron_sword',
+    'iron_helmet', 'iron_chestplate', 'iron_leggings', 'iron_boots',
+    'iron_horse_armor', 'chainmail_helmet', 'chainmail_chestplate',
+    'chainmail_leggings', 'chainmail_boots', 'iron_door', 'iron_trapdoor', 'bucket'
+  ].map(name => ({ item: 'minecraft:' + name }))
+  for (const method of ['smelting', 'blasting']) {
+    const recipeId = 'minecraft:iron_nugget_from_' + method
+    event.remove({ id: recipeId })
+    event.custom({
+      type: 'minecraft:' + method,
+      category: 'misc',
+      ingredient: recyclableItems,
+      result: 'minecraft:iron_ingot',
+      experience: 0.1,
+      cookingtime: 200
+    }).id(recipeId)
+  }
+})
